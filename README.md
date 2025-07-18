c44f86d96cbd82a6d5c842e4533096b63caffe51
unnatijais4 <unnatijais2003@gmail.com>
2025-07-18 14:36:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AI Workflow Builder
+
+Full-stack no-code/low-code AI workflow app.
---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function App() {
+  return <div>Hello AI Workflow Builder</div>;
+}
+export default App;
